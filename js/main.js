04a24cd9e71b277a06cea1d31a8b0ffd1211dee9
salma el bakkouri <elbakkourisalmaa@gmail.com>
2024-04-23 16:34:06 +0100

    added changes on menu to make it responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function addToContacts() {
     window.open('content://contacts/people/?action=edit&name=Aymd%20Agency&phone=+2120644334136', '_blank');
 }
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.querySelector('.menu-links');
+    if(menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
