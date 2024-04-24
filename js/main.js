649a4a51d9282ed532fddf0774d6ea01265e95bc
salma el bakkouri <elbakkourisalmaa@gmail.com>
2024-04-24 14:59:05 +0100

    home page, about, history, socials page and maps all responsive

--- js/main.js
+++ js/main.js
@@ -2,11 +2,36 @@ function addToContacts() {
     window.open('content://contacts/people/?action=edit&name=Aymd%20Agency&phone=+2120644334136', '_blank');
 }
 
-document.getElementById('menu-toggle').addEventListener('click', function() {
+function openMenu() {
+    console.log('button was clicked!');
     var menu = document.querySelector('.menu-links');
-    if(menu.style.display === 'block') {
-        menu.style.display = 'none';
+    var button = document.querySelector('.menu-toggle');
+    var menuContainer = document.querySelector('.menu-container');
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+        button.classList.remove('show');
+        menuContainer.style.flexDirection = "row";  
     } else {
-        menu.style.display = 'block';
+        menu.classList.add('show');
+        button.classList.add('show');
+        menuContainer.style.flexDirection = "column";   
     }
-});
+}
+
+function setupMenuToggle() {
+    const menuToggle = document.getElementById('menu-toggle');
+    window.addEventListener('scroll', function() {
+        // Check if the page is scrolled more than 50 pixels
+        if (window.scrollY > 50) {
+            menuToggle.style.position = 'absolute';  // or 'menuToggle.style.display = 'none';' to hide it
+        } else {
+            menuToggle.style.position = 'fixed';
+        }
+    });
+}
+
+// Call setup function on page load
+window.onload = function() {
+    setupMenuToggle();
+    // other setup code can go here
+};
